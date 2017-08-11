590a3b3dccb498fb26f492df4b1c572c232c1ae1
wk-j <somnuk.wk@gmail.com>
2017-08-11 23:14:51 +0700

    Update README

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 ## Hello CLJS
 
+### Build
+
+```
+$ lein cljsbuild cone
+```
+
+### Watch
+
+```
+lein cljsbuild auto
+```
+
 ### Resources
 
 - https://coderwall.com/p/02idja/setting-up-a-clojurescript-project
--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
 (defproject hello-cljs "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2411"]]
-
   :plugins [[lein-cljsbuild "1.0.3"]]
-
   :cljsbuild {
               :builds [{:source-paths ["src/cljs"]
                         :compiler {:output-to "resources/public/core.js"}}]})
