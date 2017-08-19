c4d5fea2be4f02d02a342e6e8ffc3500b45e93ae
wk-j <somnuk.wk@gmail.com>
2017-08-19 23:38:07 +0700

    Node

--- src/hello_world/core.cljs
+++ src/hello_world/core.cljs
@@ -1,8 +1,9 @@
-(ns hello-world.core)
-
-(enable-console-print!)
-
-(println "Hello world!")
-
-(defn foo [a b]
-  (+ a b))
+(ns hello-world.core
+    (:require [cljs.nodejs :as nodejs]))
+  
+  (nodejs/enable-util-print!)
+  
+  (defn -main [& args]
+    (println "Hello world!"))
+  
+  (set! *main-cli-fn* -main)
